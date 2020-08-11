50d8da429451cd63cb34ba25fae326a9f29fefc3
sergeyfedyaev <elserjo2@gmail.com>
2020-08-11 09:25:54 +0300

    Initial Commit

---
+++ MyCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCard
+//
+//  Created by Сергей Федяев on 11.08.2020.
+//  Copyright © 2020 Sergey Fedyaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
